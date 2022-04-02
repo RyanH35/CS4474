d5b1c1b63f4914eeb3a69162dce8b6d8492b0dd2
DazhuangT <101740654+DazhuangT@users.noreply.github.com>
2022-04-02 15:57:16 -0400

    side tool bar

---
+++ CS4474/Assets/Scripts/SideToolBar.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SideToolBar : MonoBehaviour
+{
+    public bool Mute = false;
+    public bool Pause = false;
+
+    public Button Mbutton;
+    public Button Pbutton;
+
+    public Sprite PImage;
+    public Sprite UPImage;
+
+    public Sprite MImage;
+    public Sprite UImage;
+
+
+    public void MuteButton()
+    {
+        if (Mute == false)
+        {
+            AudioListener.volume = 0;
+            Mbutton.image.sprite = MImage;
+            Mute = true;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            Mbutton.image.sprite = UImage;
+            Mute = false;
+        }
+    }
+
+    public void PauseButton()
+    {
+        if(Pause == false)
+        {
+            Time.timeScale = 0;
+            Pbutton.image.sprite = PImage;
+            Pause = true;
+            print("pasued");
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Pbutton.image.sprite = UPImage;
+            Pause = false;
+            print("back");
+        }
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        print("reseted");
+    }
+}
