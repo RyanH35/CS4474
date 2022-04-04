7cbed455c6025f8fced49692aba957d4c915c3d7
xxu462 <94419600+xxu462@users.noreply.github.com>
2022-04-03 21:25:59 -0400

    these contains the respawn method in player movement file

--- CS4474/Assets/Scripts/PlayerMovement.cs
+++ CS4474/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpSpeed = 100f;
     public Rigidbody2D rb;
+    public GameObject respawnPoint0;
+    public GameObject respawnPoint1;
+    public GameObject respawnPoint2;
+    public GameObject respawnPoint3;
+
     Vector2 movement;
     Vector2 lastClickedPos;
     bool moving;
@@ -18,6 +23,8 @@ public class PlayerMovement : MonoBehaviour
 
     private RaycastHit hit;
 
+    public int level = 0;
+    
 
     // Update is called once per frame
     void Update()
@@ -45,10 +52,49 @@ public class PlayerMovement : MonoBehaviour
             moving = false;
         }
 
+        if (transform.position.y >= 44)
+        {
+            level = 3;
+        }
+        else if (transform.position.y >= 27)
+        {
+            level = 2;
+        }
+        else if (transform.position.y >= 10) {
+            level = 1;
+        }
+        print(level);
+        if (Input.GetKey(KeyCode.K)) {
+            respawn(level);
+        }
+
     }
     private void FixedUpdate()
     {
         //movements
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+    public void respawn(int lev)
+    {
+        Vector3 a = transform.position;
+        Vector3 b = respawnPoint0.transform.position;
+        Vector3 c = respawnPoint1.transform.position;
+        Vector3 d = respawnPoint2.transform.position;
+        Vector3 e = respawnPoint3.transform.position;
+        if (lev == 0)
+        {
+            transform.position = Vector3.MoveTowards(a, b, moveSpeed);
+        }
+        else if (lev == 1)
+        {
+            transform.position = Vector3.MoveTowards(a, c, moveSpeed);
+        }
+        else if (lev == 2)
+        {
+            transform.position = Vector3.MoveTowards(a, d, moveSpeed);
+        } else if (lev == 3) {
+            transform.position = Vector3.MoveTowards(a, e, moveSpeed);
+        }
+    }
+
 }
